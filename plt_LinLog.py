fc66343436eb69fbb934c9a5b6ec2e74e8ed3a14
julianZeitler <julian.zeitler@zeiss.com>
2021-02-18 15:09:20 +0100

    custom plot function for LinLog

--- plt_LinLog.py
+++ plt_LinLog.py
@@ -1,9 +1,20 @@
+from numpy import asarray
+
 from plt_base import Base
 
 
 class LinLog(Base):
-    def __init__(self,
-                 *args,
-                 **kwargs):
+    def plot(self, ax, data):
+        if not isinstance(self.plots[0], list):
+            self.plots = [self.plots]
 
-        super().__init__(*args, **kwargs)
+        for plot in self.plots:
+            ax.plot(asarray(data[plot[0].strip()].values),
+                    asarray(data[plot[1].strip()].values),
+                    label=data[plot[1].strip()].name)
+
+        ax.set_yscale('log')
+        ax.set_xlabel(self.x_label)
+        ax.set_ylabel(self.y_label)
+        ax.set_title(self.title)
+        ax.legend(loc=self.legend)
