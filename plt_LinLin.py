a921e313d8d27b8dcdef3c4cec1a345295812cc6
julianZeitler <julian.zeitler@zeiss.com>
2021-02-22 13:18:57 +0100

    updated naming for private methods

--- plt_LinLin.py
+++ plt_LinLin.py
@@ -2,5 +2,5 @@ from plt_base import Base
 
 
 class LinLin(Base):
-    # LinLin inherits __init__() method from Base class
+    # LinLin inherits __init__() and plot methods from Base class
     pass
