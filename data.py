0b5cf143d895c77cf7a48e58bb542a1e4d734fc8
julianZeitler <julian.zeitler@zeiss.com>
2021-02-24 10:29:38 +0100

    histogram

--- data.py
+++ data.py
@@ -4,17 +4,20 @@ from scipy.io import savemat
 t_incremental = np.linspace(0.1, 20.1, 200)
 rad = np.linspace(0, 2, 200)
 theta = 2 * np.pi * rad
+mu, sigma = 100, 15
 
 raw_force = np.tan(t_incremental) * ((np.random.rand(1, len(t_incremental)) + 0.5) * 0.5)
 current = np.sqrt(t_incremental) * ((np.random.rand(1, len(t_incremental)) + 0.5) * 0.5)
 raw_voltage = np.log(t_incremental) * ((np.random.rand(1, len(t_incremental)) + 0.5) * 0.5)
+x_hist = mu + sigma*np.random.randn(10000)
 
 d1 = {'plot_data': {'data': {'raw_force': {'values': raw_force, 'name': 'Raw Force', 'unit': 'N'},
                              'current': {'values': current, 'name': 'Current', 'unit': 'A'},
                              'raw_voltage': {'values': raw_voltage, 'name': 'Raw Voltage', 'unit': 'V'},
                              'rad': {'values': rad, 'name': 'rad', 'unit': 'm'},
                              'theta': {'values': theta, 'name': 'theta', 'unit': 'rad'},
-                             't_incremental': {'values': t_incremental, 'name': 'Time incremental', 'unit': 's'}},
+                             't_incremental': {'values': t_incremental, 'name': 'Time incremental', 'unit': 's'},
+                             'hist': {'values': x_hist, 'name': 'hist', 'unit': 'IQ'}},
 
                     'meta': {'timestamp_last_sample': 12345.6789,
                              'location': 'Deutschland, Oberkochen',
@@ -43,7 +46,12 @@ d1 = {'plot_data': {'data': {'raw_force': {'values': raw_force, 'name': 'Raw For
 
                                                 {'subplot': [{'plots': [['theta', 'rad']],
                                                               'plot_type': 'Polar',
-                                                              'title': 'example polar plot'}]}],
+                                                              'title': 'example polar plot'}]},
+
+                                                {'subplot': [{'plots': ['hist'],
+                                                              'plot_type': 'Hist',
+                                                              'title': 'example for histogram',
+                                                              'bins': 50}]}],
                                      'linkaxes': [[0, 0], [0, 1], [1, 0]]}}}}
 
 
--- plt_Polar.py
+++ plt_Polar.py
@@ -6,7 +6,7 @@ from plt_base import Base
 
 class Polar(Base):
     def plot(self, ax, data):
-        ax = subplot(111, projection='polar')
+        ax = subplot(projection='polar')
 
         if not isinstance(self.plots[0], list):
             self.plots = [self.plots]
@@ -16,4 +16,8 @@ class Polar(Base):
                     asarray(data[plot[1].strip()].values),
                     label=data[plot[1].strip()].name)
 
+        ax.set_xlabel(self.x_label)
+        ax.set_ylabel(self.y_label)
+        ax.set_title(self.title)
+        ax.legend(loc=self.legend)
         ax.grid(self.grid)
