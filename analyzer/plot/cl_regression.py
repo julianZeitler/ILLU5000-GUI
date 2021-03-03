a6153ccf1207e9b5f7d6ce6cd82fd436b8b28103
julianZeitler <julian.zeitler@zeiss.com>
2021-03-03 12:14:18 +0100

    changed todo

--- analyzer/plot/cl_regression.py
+++ analyzer/plot/cl_regression.py
@@ -49,9 +49,7 @@ class Linear(Regression):
 
 class Exponential(Regression):
     """Create an exponential regression
-    .. todo::
-       currently only two parameters are calculated of the form y = a(e^bx)
-       add a third parameter c : y = a(e^bx) + c
+    .. todo:: currently only two parameters are calculated of the form y = a(e^bx) add a third parameter c : y = a(e^bx) + c
     """
     def fit(self):
         """Calculate the parameters with the method of smallest error squares"""
