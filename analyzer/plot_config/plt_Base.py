50f28051269c8482c983efcf49b910d84b80a90c
julianZeitler <julian.zeitler@zeiss.com>
2021-03-03 15:25:01 +0100

    changed plt_Base.py

--- analyzer/plot_config/plt_Base.py
+++ analyzer/plot_config/plt_Base.py
@@ -30,7 +30,7 @@ class Base:
         :param grid: toggle grid
         :type grid: bool
         :param plot_type: name of class from which `plot()` should be inherited
-        (it is sufficient to write the name without 'plt_')
+        (it is sufficient to write the name without )
         :type plot_type: str
         :param regression: optionally add regression type
         :type regression: str
