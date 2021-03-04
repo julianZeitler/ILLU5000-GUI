7f62428c1ad270c63ea2647e147d4b5764856f36
julianZeitler <julian.zeitler@zeiss.com>
2021-03-04 16:30:53 +0100

    build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
 git pull origin main
+rm -rf _build/html/*
+rm -rf _build/latex/*
 sphinx-build -b html . _build/html
 sphinx-build -b latex . _build/latex
 zip -r _build/DataAnalyzer.zip _build/latex
