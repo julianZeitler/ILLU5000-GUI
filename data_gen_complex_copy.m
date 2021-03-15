207b8b2fa1d1868ec7bcf7d4123e3052f6619ccf
julianZeitler <julian.zeitler@zeiss.com>
2021-03-15 20:49:25 +0100

    plot

--- data_gen_complex_copy.m
+++ data_gen_complex_copy.m
@@ -21,39 +21,42 @@ plot_data.meta = struct('timestamp_last_sample', 123,
 
 plot_data.plot.raw.figure(1).subplot_cols = 2
 plot_data.plot.raw.figure(1).subplot_rows = 2
-plot_data.plot.raw.figure(1).subplot = {struct('plots', {{'time', 'raw_force'},
-                                                                   {'time', 'current'}},
-                                                         'x_label', 'X-Axis of plot 1',
-                                                         'plot_type', 'LinLin',
-                                                         'title', 'subplot 1',
-                                                         'legend', 'lower left'),
-                                                  struct('plots', {'time', 'raw_voltage'},
-                                                         'plot_type', 'LinLog'),
-                                                  struct('plots', {'time', 'raw_force'},
-                                                         'grid', false),
-                                                  struct('plots', {'time', 'raw_voltage'})}
+plot_data.plot.raw.figure(1).subplot(1).plots = {{'time', 'raw_force'},
+                                                 {'time', 'current'}}
+plot_data.plot.raw.figure(1).subplot(1).x_label = 'X-Axis of plot 1'
+plot_data.plot.raw.figure(1).subplot(1).plot_type = 'LinLin'
+plot_data.plot.raw.figure(1).subplot(1).title = 'subplot 1'
+plot_data.plot.raw.figure(1).subplot(1).legend = 'lower left'
+
+plot_data.plot.raw.figure(1).subplot(2).plots = {'time', 'raw_voltage'}
+plot_data.plot.raw.figure(1).subplot(2).plot_type = 'LinLog'
+
+plot_data.plot.raw.figure(1).subplot(3).plots = {'time', 'raw_force'}
+plot_data.plot.raw.figure(1).subplot(3).grid = false
+
+plot_data.plot.raw.figure(1).subplot(4).plots = {'time', 'current'}
 
 plot_data.plot.raw.figure(2).subplot_rows = 2
-plot_data.plot.raw.figure(2).subplot = {struct('plots', {'time', 'raw_voltage'}),
-                                                   struct('plots', {'time', 'current'},
-                                                          'regression', 'Root')}
+plot_data.plot.raw.figure(2).subplot(1).plots = {'time', 'raw_voltage'}
+plot_data.plot.raw.figure(2).subplot(2).plots = {'time', 'current'}
+plot_data.plot.raw.figure(2).subplot(2).regression = 'Root'
 
-plot_data.plot.raw.figure(3).subplot = {struct('plots', {'phi', 'time'},
-                                                          'plot_type', 'Polar',
-                                                          'title', 'example of polar plot')}
+plot_data.plot.raw.figure(3).subplot(1).plots = {'phi', 'time'},
+plot_data.plot.raw.figure(3).subplot(1).plot_type = 'Polar'
+plot_data.plot.raw.figure(3).subplot(1).title = 'example of polar plot'
+
 plot_data.plot.raw.figure(4).subplot_cols = 2
-plot_data.plot.raw.figure(4).subplot = {struct('plots', {'hist'},
-                                                          'x_label', 'IQ',
-                                                          'plot_type', 'Hist',
-                                                          'title', 'example for histogram',
-                                                          'bins', 20,
-                                                          'x_label', 'IQ'),
+plot_data.plot.raw.figure(4).subplot(1).plots = {'hist'}
+plot_data.plot.raw.figure(4).subplot(1).x_label = 'IQ'
+plot_data.plot.raw.figure(4).subplot(1).plot_type = 'Hist'
+plot_data.plot.raw.figure(4).subplot(1).title = 'example of histogram'
+plot_data.plot.raw.figure(4).subplot(1).bins = 20
+plot_data.plot.raw.figure(4).subplot(2).plots = {'Hist'}
+plot_data.plot.raw.figure(4).subplot(2).plot_type = 'Hist'
+plot_data.plot.raw.figure(4).subplot(2).title = 'second histogram'
+plot_data.plot.raw.figure(4).subplot(2).bins = 50
+plot_data.plot.raw.figure(4).subplot(2).x_label = 'IQ'
 
-                                                   struct('plots', {'hist'},
-                                                          'plot_type', 'Hist',
-                                                          'title', 'second histogram',
-                                                          'bins', 50,
-                                                          'x_label', 'IQ')}
 plot_data.plot.raw.linkaxes = {{0, 0}, {0, 2}, {1, 0}}
 
 save('-v6', 'data.mat', 'plot_data');
