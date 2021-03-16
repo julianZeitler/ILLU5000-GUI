60ff4511d357f6e72bc5c2102fb8ea39e9783051
julianZeitler <julian.zeitler@zeiss.com>
2021-03-16 09:32:36 +0100

    rename

--- data_gen_complex.m
+++ data_gen_complex.m
@@ -1,60 +1,79 @@
-
 phi = 12 * pi * [0 : 999] / 1000;
 mu = 100;
 sigma = 15;
 
+time = [0 : 999] / 1000;
 raw_force = tan(time);
 current = sqrt(time);
 raw_voltage = log(time);
-x_hist = mu + sigma * randi(10000);
+x_hist = mu + sigma * randi(200, 1000)
 
-plot_data = struct('data', struct('raw_force', struct('values', raw_force, 'name', 'Raw Force', 'unit', 'N'),
-                                  'current', struct('values', current, 'name', 'Current', 'unit', 'A'),
-                                  'raw_voltage', struct('values', raw_voltage, 'name', 'Raw Voltage', 'unit', 'V'),
-                                  'phi', struct('values', phi, 'name', 'Phi', 'unit', 'rad'),
-                                  'time', struct('values', time, 'name', 'time', 'unit', 's'),
-                                  'hist', struct('values', x_hist, 'name', 'hist', 'unit', 'IQ')),
+plot_data.data.raw_force.values = raw_force;
+plot_data.data.raw_force.name = 'Raw Force';
+plot_data.data.raw_force.unit = 'N';
 
-                   'meta', struct('timestamp_last_sample', 123,
-                                  'location', 'Deutschland, Oberkochen',
-                                  'machine', 'some machine',
-                                  'worker', 'Max Mustermann'),
+plot_data.data.current.values = current;
+plot_data.data.current.name = 'Current';
+plot_data.data.current.unit = 'A';
 
-                   'plot', struct('raw', struct('figure', {struct('subplot_cols', 2,
-                                                                  'subplot_rows', 2,
-                                                                  'subplot', {struct('plots', {{'time', 'raw_force'},
-                                                                                               {'time', 'current'}},
-                                                                                     'x_label', 'X-Axis of plot 1',
-                                                                                     'plot_type', 'LinLin',
-                                                                                     'title', 'subplot 1',
-                                                                                     'legend', 'lower left'),
-                                                                              struct('plots', {'time', 'raw_voltage'},
-                                                                                     'plot_type', 'LinLog'),
-                                                                              struct('plots', {'time', 'raw_force'},
-                                                                                     'grid', false),
-                                                                              struct('plots', {'time', 'raw_voltage'})}),
+plot_data.data.raw_voltage.values = raw_voltage;
+plot_data.data.raw_voltage.name = 'Raw Voltage';
+plot_data.data.raw_voltage.unit = 'V';
 
-                                                           struct('subplot_rows', 2,
-                                                                  'subplot', {struct('plots', {'time', 'raw_voltage'}),
-                                                                              struct('plots', {'time', 'current'},
-                                                                                     'regression', 'Root')}),
+plot_data.data.phi.values = phi;
+plot_data.data.phi.name = 'phi';
+plot_data.data.phi.unit = 'rad';
 
-                                                           struct('subplot', {struct('plots', {'phi', 'time'},
-                                                                                     'plot_type', 'Polar',
-                                                                                     'title', 'example of polar plot')}),
-                                                           struct('subplot_cols', 2,
-                                                                  'subplot', {struct('plots', {'hist'},
-                                                                                     'x_label', 'IQ',
-                                                                                     'plot_type', 'Hist',
-                                                                                     'title', 'example for histogram',
-                                                                                     'bins', 20,
-                                                                                     'x_label', 'IQ'),
+plot_data.data.time.values = time;
+plot_data.data.time.name = 'time';
+plot_data.data.time.unit = 's';
 
-                                                                              struct('plots', {'hist'},
-                                                                                     'plot_type', 'Hist',
-                                                                                     'title', 'second histogram',
-                                                                                     'bins', 50,
-                                                                                     'x_label', 'IQ')})},
-                                                'linkaxes', {{0, 0}, {0, 2}, {1, 0}})))
+plot_data.data.hist.values = x_hist;
+plot_data.data.hist.name = 'hist';
+plot_data.data.hist.unit = 'IQ';
+
+plot_data.meta.timestamp_last_sample = 123;
+plot_data.meta.location = 'Deutschland, Oberkochen';
+plot_data.meta.machine = 'some machine';
+plot_data.meta.worker = 'Max Mustermann';
+
+plot_data.plot.raw.figure{1}.subplot_cols = 2;
+plot_data.plot.raw.figure{1}.subplot_rows = 2;
+plot_data.plot.raw.figure{1}.subplot{1}.plots = {{'time', 'raw_force'}, {'time', 'current'}};
+plot_data.plot.raw.figure{1}.subplot{1}.x_label = 'X-Axis of plot 1';
+plot_data.plot.raw.figure{1}.subplot{1}.plot_type = 'LinLin';
+plot_data.plot.raw.figure{1}.subplot{1}.title = 'subplot 1';
+plot_data.plot.raw.figure{1}.subplot{1}.legend = 'lower left';
+
+plot_data.plot.raw.figure{1}.subplot{2}.plots = {'time', 'raw_voltage'};
+
+plot_data.plot.raw.figure{1}.subplot{3}.plots = {'time', 'raw_force'};
+plot_data.plot.raw.figure{1}.subplot{3}.grid = false;
+
+plot_data.plot.raw.figure{1}.subplot{4}.plots = {'time', 'current'};
+plot_data.plot.raw.figure{1}.subplot{4}.plot_type = 'LinLog';
+
+plot_data.plot.raw.figure{2}.subplot_rows = 2;
+plot_data.plot.raw.figure{2}.subplot{1}.plots = {'time', 'raw_voltage'};
+plot_data.plot.raw.figure{2}.subplot{2}.plots = {'time', 'current'};
+plot_data.plot.raw.figure{2}.subplot{2}.regression = 'Root';
+
+plot_data.plot.raw.figure{3}.subplot{1}.plots = {'phi', 'time'};
+plot_data.plot.raw.figure{3}.subplot{1}.plot_type = 'Polar';
+plot_data.plot.raw.figure{3}.subplot{1}.title = 'example of polar plot';
+
+plot_data.plot.raw.figure{4}.subplot_cols = 2;
+plot_data.plot.raw.figure{4}.subplot{1}.plots = {'hist'};
+plot_data.plot.raw.figure{4}.subplot{1}.x_label = 'IQ';
+plot_data.plot.raw.figure{4}.subplot{1}.plot_type = 'Hist';
+plot_data.plot.raw.figure{4}.subplot{1}.title = 'example of histogram';
+plot_data.plot.raw.figure{4}.subplot{1}.bins = 20;
+plot_data.plot.raw.figure{4}.subplot{2}.plots = {'hist'};
+plot_data.plot.raw.figure{4}.subplot{2}.plot_type = 'Hist';
+plot_data.plot.raw.figure{4}.subplot{2}.title = 'second histogram';
+plot_data.plot.raw.figure{4}.subplot{2}.bins = 50;
+plot_data.plot.raw.figure{4}.subplot{2}.x_label = 'IQ';
+
+plot_data.plot.raw.linkaxes = {{0, 0}, {0, 2}, {1, 0}};
 
 save('-v6', 'data.mat', 'plot_data');
